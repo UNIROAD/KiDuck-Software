30a866832db6efc041405b496beed8683b608f57
edong6768 <edong6768@gmail.com>
2022-06-30 01:34:58 +0900

    Tried to chnage button input method and failed

--- Global_State.h
+++ Global_State.h
@@ -9,10 +9,19 @@ bool prev3 = false, curr3 = false;
 bool prev4 = false, curr4 = false;
 bool prev5 = false, curr5 = false;
 
+// int prevb = 0b0000, currb = 0b0000, pressb = 0;
+
 bool fall_edge(int16_t pin_num, bool* prev, bool* curr){
   *prev = *curr;
   *curr = (digitalRead(pin_num))?true:false;
   return *prev && !*curr;
+}
+
+void setButtonPinMode(){
+  for(int i=2;i<6;i++) pinMode(i, INPUT_PULLUP);
+
+  //attachInterrupt(digitalPinToInterrupt(BUTTON), cleartest, FALLING);
+  //attachInterrupt(digitalPinToInterrupt(9), interruptButton2, FALLING);
 }
 
 bool buttonPressDetect(){
@@ -25,9 +34,12 @@ bool buttonPressDetect(){
   return false;
 }
 
-void setButtonPinMode(){
-  for(int i=2;i<6;i++) pinMode(i, INPUT_PULLUP);
-
-  //attachInterrupt(digitalPinToInterrupt(BUTTON), cleartest, FALLING);
-  //attachInterrupt(digitalPinToInterrupt(9), interruptButton2, FALLING);
-}
+// int buttonPress(){
+//   prevb = currb;
+//   currb = (PORTD & 0b111100)>>2;
+//   pressb = prevb & ~currb;
+//   for(int i=0; i<4; i++){
+//     if(1&(pressb>>i)) return i;
+//   }
+//   return 0;
+// }
