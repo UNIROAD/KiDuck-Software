c7bfa0839e0a01aec88441b06c18c09866054f20
edong6768 <edong6768@gmail.com>
2022-09-25 16:11:20 +0900

    Add elapsed_time_check

--- Global_State.h
+++ Global_State.h
@@ -4,19 +4,46 @@
 #define ARD
 #endif
 
-bool previn[] = {false, false, false, false};
-bool currin[] = {false, false, false, false};
+#ifndef GLST
+#include <vector>
 
+using namespace std;
+
+// Game Datas
 string user_name;
 int user_age;
 
+int character;
+int growth;
+int points;
+
+bool previn[] = {false, false, false, false};
+bool currin[] = {false, false, false, false};
+
 void setButtonPinMode(){
+  // pin 9, 8, 7, 6
   for(int i=6;i<10;i++) pinMode(i, INPUT_PULLUP);
 }
 
-// pin 5, 4, 3, 2
 bool fall_edge(int num){
   previn[num] = currin[num];
   currin[num] = (bool)digitalRead(num+6);
   return previn[num] && !currin[num];
 }
+
+bool t_flag=false;
+unsigned long start_t, crit_t=100;
+
+bool elapsed_time_check(){
+    if(!t_flag){
+        t_flag = true;
+        start_t = millis();
+    }else if(millis()-start_t>=crit_t){
+        t_flag = false;
+        return true;
+    }
+    return false;
+}
+
+#define GLST
+#endif
