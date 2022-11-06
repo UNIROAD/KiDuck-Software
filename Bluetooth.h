be2b428a52ddac16d87fa4b47a65df9495c0db8d
edong6768 <edong6768@gmail.com>
2022-11-06 20:40:59 +0900

    Fix setupBlueToothConnection and Add Global_State header

--- Bluetooth.h
+++ Bluetooth.h
@@ -1,15 +1,17 @@
 #include <SoftwareSerial.h>   //Software Serial Port
 #include <ArduinoSTL.h>
-#define RxD 7
-#define TxD 8
 
-#define MASTER 0    //change this macro to define the Bluetooth as Master or not 
+#include "Global_State.h"
+
+#ifndef BLE
+
+#define RxD 4
+#define TxD 5
 
 SoftwareSerial blueToothSerial(RxD, TxD); //the software serial port
 
 char recv_str[100];
 
-String kiduckName = "TEST";
 int dataCount = 8;
 int data[8][3] = { // first index: day, second index: 0-step count 1-drink 2-communication
   {2991, 2000, 6},
@@ -26,58 +28,13 @@ int threshold[3] = {10000, 1500, 5};
 String password = "abcdefg";
 int emergencyAlarm = 0;
 
+int escape_flag = 0;
+
 #define COMM_INFO 0
 #define COMM_GROWTH 1
 #define COMM_NAME 2
 
-void bleSetup()
-{
-  pinMode(RxD, INPUT);    //UART pin for Bluetooth
-  pinMode(TxD, OUTPUT);   //UART pin for Bluetooth
-  Serial.println("\r\nPower on!!");
-  setupBlueToothConnection(); //initialize Bluetooth
-  //this block is waiting for connection was established.
-  while (1){
-    if (recvMsg(100) == 0){
-      if (recvcmp((char *)"OK+CONN")==0){
-        Serial.println("connected\r\n");
-        break;
-      }
-    }
-    delay(200);
-  }
-}
-
-void syncApp(){
-  delay(200);
-  if (recvMsg(1000) == 0){
-    if      (recvcmp((char *)"AppConnected")==0)    send(COMM_INFO);
-    else if (recvcmp((char *)"InitGrowth")==0)      send(COMM_GROWTH);
-    else if (recvcmp((char *)"SetGrowth")==0)       recv(COMM_GROWTH);
-    else if (recvcmp((char *)"InitName")==0)        send(COMM_NAME);
-    else if (recvcmp((char *)"SetName")==0)         recv(COMM_NAME);
-    Serial.print("recv: ");
-    Serial.println((char *)recv_str);
-  }
-}
-
-void send(int c){
-    if (sendFunc(c) == -1) 
-        blueToothSerial.print("Fail to send        ");
-}
-
-void recv(int c){
-    blueToothSerial.print("ACK");
-    if (recvMsg(1000) == 0) {
-        if (parseFunc(c) == 0)
-            blueToothSerial.print("SUCCESS");
-        else
-            blueToothSerial.print("FAIL");
-    }
-}
-
-int recvcmp(char *str) {return strcmp((char *)recv_str, str);}
-
+//########################## string ##########################//
 //used for compare two string, return 0 if one equals to each other
 int strcmp(char *a, char *b){
   for(unsigned int ptr = 0; a[ptr] != '\0'; ptr++){
@@ -86,6 +43,9 @@ int strcmp(char *a, char *b){
   return 0;
 }
 
+//compare given string with recv_str
+int recvcmp(char *str) {return strcmp((char *)recv_str, str);}
+
 int strcmp2(char *a, const char *b){
   for(unsigned int ptr = 0; a[ptr] != '\0'; ptr++){
     if (a[ptr] != b[ptr]) return -1;
@@ -93,60 +53,7 @@ int strcmp2(char *a, const char *b){
   return 0;
 }
 
-//configure the Bluetooth through AT commands
-int setupBlueToothConnection()
-{
-  Serial.print("Setting up Bluetooth link\r\n");
-  delay(2000);//wait for module restart
-  blueToothSerial.begin(9600);
-
-  //wait until Bluetooth was found
-  while (1){
-    if (sendBlueToothCommand("AT") == 0){
-      if (recvcmp((char *)"OK") == 0){
-        Serial.println("Bluetooth exists\r\n");
-        break;
-      }
-    }
-    delay(500);
-  }
-
-  //configure the Bluetooth
-  sendBlueToothCommand("AT+DEFAULT");//restore factory configurations
-  delay(2000);
-  sendBlueToothCommand("AT+NAME?");
-  sendBlueToothCommand("AT+NAMEKIDUCK");
-  sendBlueToothCommand("AT+ROLES");//set to slave mode
-  sendBlueToothCommand("AT+RESTART");//restart module to take effect
-  delay(2000);//wait for module restart
-
-  //check if the Bluetooth always exists
-  if (sendBlueToothCommand("AT") == 0){
-    if (recvcmp((char *)"OK") == 0){
-      Serial.print("Setup complete\r\n\r\n");
-      return 0;
-    }
-  }
-  return -1;
-}
-
-//send command to Bluetooth and return if there is a response received
-int sendBlueToothCommand(char command[])
-{
-  Serial.print("send: ");
-  Serial.print(command);
-  Serial.println("");
-
-  blueToothSerial.print(command);
-  delay(300);
-
-  if (recvMsg(200) != 0) return -1;
-
-  Serial.print("recv: ");
-  Serial.print(recv_str);
-  Serial.println("");
-  return 0;
-}
+//########################## Bluetooth ##########################//
 
 //receive message from Bluetooth with time out
 int recvMsg(unsigned int timeout)
@@ -178,9 +85,66 @@ int recvMsg(unsigned int timeout)
   return 0;
 }
 
+//send command to Bluetooth and return if there is a response received
+int sendBlueToothCommand(char command[])
+{
+  Serial.print("send: ");
+  Serial.print(command);
+  Serial.println("");
 
+  blueToothSerial.print(command);
+  delay(300);
+
+  if (recvMsg(200) != 0) return -1;
+
+  Serial.print("recv: ");
+  Serial.print(recv_str);
+  Serial.println("");
+  return 0;
+}
+
+
+//configure the Bluetooth through AT commands
+int setupBlueToothConnection()
+{
+  Serial.print("Setting up Bluetooth link\r\n");
+  delay(2000);//wait for module restart
+  blueToothSerial.begin(9600);
+
+  //wait until Bluetooth was found
+  while (1){
+    if (sendBlueToothCommand("AT") == 0){
+      if (recvcmp((char *)"OK") == 0){
+        Serial.println("Bluetooth exists\r\n");
+        break;
+      }
+    }
+    delay(500);
+  }
+
+  //configure the Bluetooth
+  sendBlueToothCommand("AT+RENEW");//restore factory configurations
+  delay(2000);
+  sendBlueToothCommand("AT+NAME?");
+  sendBlueToothCommand("AT+NAMEKIDUCK");
+  sendBlueToothCommand("AT+ROLE0");//set to slave mode
+  sendBlueToothCommand("AT+RESTART");//restart module to take effect
+  delay(2000);//wait for module restart
+
+  //check if the Bluetooth always exists
+  if (sendBlueToothCommand("AT") == 0){
+    if (recvcmp((char *)"OK") == 0){
+      Serial.print("Setup complete\r\n\r\n");
+      return 0;
+    }
+  }
+  return -1;
+}
+
+
+//########################## Send ##########################//
 int sendKiDuckInfo() {
-  String sendName = kiduckName;
+  String sendName = user_name;
   if (sendName.length() > 20)
     return -1;
 
@@ -228,8 +192,8 @@ int sendKiDuckGrowth() {
   return 0;
 }
 
-int sendKiDuckName() {
-  String sendName = kiduckName;
+int senduser_name() {
+  String sendName = user_name;
   if (sendName.length() > 20)
     return -1;
 
@@ -241,6 +205,17 @@ int sendKiDuckName() {
   return 0;
 }
 
+int sendFunc(int c){
+    switch(c){
+        case 0: return sendKiDuckInfo();
+        case 1: return sendKiDuckGrowth();
+        case 2: return senduser_name();
+        default: return -1;
+    }
+}
+
+
+//########################## Parse ##########################//
 int parseGrowth() {
   String strStepCriteria = "";
   String strDrinkCriteria = "";
@@ -273,7 +248,7 @@ int parseName() {
   String newName = "";
   newName = String((char *)recv_str);
   Serial.println(newName);
-  kiduckName = newName;
+  user_name = newName;
   return 0;
 }
 
@@ -286,11 +261,53 @@ int parseFunc(int c){
     }
 }
 
-int sendFunc(int c){
-    switch(c){
-        case 0: return sendKiDuckInfo();
-        case 1: return sendKiDuckGrowth();
-        case 2: return sendKiDuckName();
-        default: return -1;
-    }
+void send(int c){
+    if (sendFunc(c) == -1) 
+        blueToothSerial.print("Fail to send        ");
 }
+
+void recv(int c){
+    blueToothSerial.print("ACK");
+    if (recvMsg(1000)) return;
+    if (parseFunc(c) == 0)
+        blueToothSerial.print("SUCCESS");
+    else
+        blueToothSerial.print("FAIL");
+}
+
+
+void bleSetup()
+{
+  pinMode(RxD, INPUT);    //UART pin for Bluetooth
+  pinMode(TxD, OUTPUT);   //UART pin for Bluetooth
+  Serial.println("\r\nPower on!!");
+  setupBlueToothConnection(); //initialize Bluetooth
+  //this block is waiting for connection was established.
+  while (1){
+    if (recvMsg(100) == 0){
+      if (recvcmp((char *)"OK+CONN")==0){
+        Serial.println("connected\r\n");
+        break;
+      }
+    }
+    delay(200);
+  }
+}
+
+void syncApp(){
+  delay(200);
+  if (recvMsg(1000) == 0){
+    if      (recvcmp((char *)"AppConnected")==0)    send(COMM_INFO);
+    else if (recvcmp((char *)"InitGrowth")==0)      send(COMM_GROWTH);
+    else if (recvcmp((char *)"SetGrowth")==0)       recv(COMM_GROWTH);
+    else if (recvcmp((char *)"InitName")==0)        send(COMM_NAME);
+    else if (recvcmp((char *)"SetName")==0)         recv(COMM_NAME);
+    else if (recvcmp((char *)"OK+LOST")==0)         escape_flag = 1;
+
+    Serial.print("recv: ");
+    Serial.println((char *)recv_str);
+  }
+}
+
+#define BLE
+#endif
